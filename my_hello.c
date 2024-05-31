4346dc1625168cfc8950542ef47311655c977fb4
TsutomuInoue <58171298+TsutomuInoue@users.noreply.github.com>
2024-05-31 17:30:20 +0900

    Update my_hello.c

--- my_hello.c
+++ my_hello.c
@@ -9,3 +9,14 @@ int main(int argc, char**argv) {
   }
   return 0; 
 }
+int dommy(int argc, char**argv) {
+  for (int i; i < 10; i++) {
+    printf("Hello,World"); 
+  }
+  /**
+    @wi MYDO-265
+  */
+  while (-1) {
+  }
+  return 0; 
+}
